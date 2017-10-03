d302141b258e1aab02478e0989aa60574ca602eb
alexandrawaite <alexandrawaite@gmail.com>
2017-10-03 16:29:18 -0700

    create user table

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -1,4 +1,13 @@
 DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id serial,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
+);
 
 CREATE TABLE contacts (
   id serial,
